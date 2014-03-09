bb874a1fa484aaa0969f797f6e19c9bec84b660d
Star Brilliant <m13253@hotmail.com>
2014-03-09 15:56:56 +0800

    glottal_source.m: Octave starts an array from 1

--- glottal-source/glottal_source.m
+++ glottal-source/glottal_source.m
@@ -2,7 +2,7 @@ function res = glottal_source(f0, nsamples, len)
     spct = zeros(len*nsamples, 1);
     maxtimes = nsamples/f0/2;
     for i = 1:maxtimes
-        spct(f0*len*i) = -1/2j*len*nsamples/i/i;  % -12dB/octave
+        spct(f0*len*i+1) = -1/2j*len*nsamples/i/i;  % -12dB/octave
     end
     res = real(ifft(spct));
 endfunction
