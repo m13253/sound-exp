09b11fa2bb0673dd8402cfe11f477caa7c636a1d
Star Brilliant <m13253@hotmail.com>
2014-03-09 12:09:28 +0800

    glottal_source.m

--- glottal-source/glottal_source.m
+++ glottal-source/glottal_source.m
@@ -2,7 +2,7 @@ function res = glottal_source(f0, nsamples, len)
     spct = zeros(len*nsamples, 1);
     maxtimes = nsamples/f0/2;
     for i = 1:maxtimes
-        spct(f0*len*i) = -2j*len*nsamples/(4**i);
+        spct(f0*len*i) = -2j*len*nsamples*(4**-i);
     end
     res = real(ifft(spct));
 endfunction
